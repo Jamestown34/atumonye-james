699f327186bd56702993dcb8975a71ec02ded1b4
Atumonye James <154873745+Jamestown34@users.noreply.github.com>
2025-08-08 08:19:21 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import GA4Tracker from './GA4Tracker';
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <GA4Tracker />
+    <App />
+  </>
+);
